1f67511342b57225dadc873b88a3240350be9110
Haydn Dufrene <haydn.dufrene@affirm.com>
2014-07-29 15:57:56 -0700

    api: add watchlist support

--- blockscore/client.py
+++ blockscore/client.py
@@ -3,10 +3,12 @@ from .http_client import HttpClient
 # Assign all the api classes
 from .api.verification import Verification
 from .api.question_set import QuestionSet
+from api.watchlist import Watchlist
+
 
 class Client():
-
-	def __init__(self, auth = {}, options = {}):
-		self.http_client = HttpClient(auth, options)
-		self.verification = Verification(self.http_client)
-		self.question_set = QuestionSet(self.http_client)
+    def __init__(self, auth={}, options={}):
+        self.http_client = HttpClient(auth, options)
+        self.verification = Verification(self.http_client)
+        self.question_set = QuestionSet(self.http_client)
+        self.watchlist = Watchlist(self.http_client)
